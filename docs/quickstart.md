1a7442cf9af5896d8d64b3a2d013833315992e17
h_gxi <h_gxi@foxmail.com>
2024-05-24 13:59:34 +0800

    创建新组件库

---
+++ docs/quickstart.md
@@ -0,0 +1,17 @@
+# 快速上手
+
+### 安装
+
+```bash
+# 通过 npm
+npm i vant-common
+
+# 通过 yarn
+yarn add vant-common
+
+# 通过 pnpm
+pnpm add vant-common
+
+# 通过 bun
+bun add vant-common
+```
---
+++ vant.config.mjs
@@ -0,0 +1,40 @@
+export default {
+  name: 'vant-common',
+  build: {
+    css: {
+      preprocessor: 'sass',
+    },
+    site: {
+      publicPath: '/vant-common/',
+    },
+    packageManager: 'npm'
+  },
+  site: {
+    title: 'vant-common',
+    logo: 'https://fastly.jsdelivr.net/npm/@vant/assets/logo.png',
+    nav: [
+      {
+        title: '开发指南',
+        items: [
+          {
+            path: 'home',
+            title: '介绍',
+          },
+          {
+            path: 'quickstart',
+            title: '快速上手',
+          },
+        ],
+      },
+      {
+        title: '基础组件',
+        items: [
+          {
+            path: 'demo-button',
+            title: 'DemoButton 按钮',
+          },
+        ],
+      },
+    ],
+  },
+};
