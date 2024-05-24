9dd1d648604e8b517e08564e08f4b5a5fb865c1c
h_gxi <h_gxi@foxmail.com>
2024-05-24 14:57:58 +0800

    11

---
+++ src/header/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div v-if="isSlotsLoaded" class="m-header" :style="styleObj">
+    <div ref="headerRef" class="m-header--fixed">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, computed, defineComponent, nextTick, onMounted } from 'vue';
+import { useChildren } from '@vant/use';
+import { useRect } from '@vant/use';
+import { headerProps } from './props';
+
+const name = 'm-header';
+export const HEADER_KEY = Symbol(name);
+
+export default defineComponent({
+  name,
+  props: headerProps,
+  emits: ['onResize'],
+  setup(props, { emit, slots }) {
+    const { linkChildren } = useChildren(HEADER_KEY);
+
+    const offsetHeight = ref(46);
+    const headerRef = ref(null);
+
+    const isSlotsLoaded = computed(() => slots.default().length > 0);
+
+    const styleObj = computed(() => {
+      let style = '';
+      if (props.height) {
+        if (typeof props.height === 'number') {
+          style = `height: ${props.height}px;`;
+        } else {
+          style = `height: ${props.height};`;
+        }
+      } else {
+        if (offsetHeight.value > 0) {
+          style = `height: ${offsetHeight.value}px;`;
+        }
+      }
+      return style;
+    });
+
+    /**
+     * 重置header高度
+     */
+    const resize = () => {
+      nextTick(() => {
+        offsetHeight.value = headerRef.value?.offsetHeight;
+        emit('onResize', offsetHeight.value);
+      });
+    };
+
+    linkChildren({
+      resize,
+    });
+
+    onMounted(() => {
+      if (!props.height) {
+        nextTick(() => {
+          setTimeout(() => {
+            const rect = useRect(headerRef);
+            offsetHeight.value = rect.height + 3;
+          }, 50);
+        });
+      }
+    });
+
+    return {
+      isSlotsLoaded,
+      styleObj,
+      headerRef,
+      resize,
+    };
+  },
+});
+</script>
--- vant.config.mjs
+++ vant.config.mjs
@@ -2,16 +2,26 @@ export default {
   name: 'vant-common',
   build: {
     css: {
+      base: 'style/index.scss',
       preprocessor: 'sass',
+      removeSourceFile: true,
     },
     site: {
       publicPath: '/vant-common/',
     },
+    namedExport: false,
     packageManager: 'npm'
   },
   site: {
     title: 'vant-common',
     logo: 'https://fastly.jsdelivr.net/npm/@vant/assets/logo.png',
+    description: '基于vant3封装的自定义组件库，适用于H5开发迭代，降低开发维护成本。',
+    versions: [
+      {
+        label: 'v1',
+        link: '#',
+      },
+    ],
     nav: [
       {
         title: '开发指南',
