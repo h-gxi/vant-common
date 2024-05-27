0af98a774c7eeacdfd17034b90cdaab28c343c05
h_gxi <h_gxi@foxmail.com>
2024-05-27 15:44:44 +0800

    更新说明

--- vant.config.mjs
+++ vant.config.mjs
@@ -17,10 +17,6 @@ export default {
     logo: 'https://fastly.jsdelivr.net/npm/@vant/assets/logo.png',
     description: '基于vant3封装的自定义组件库，适用于H5开发迭代，降低开发维护成本。',
     versions: [
-      {
-        label: 'v1',
-        link: '#',
-      },
     ],
     nav: [
       {
