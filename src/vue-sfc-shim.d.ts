f233a5df47dec2f4054bdce56a2a028e2bd7af40
h_gxi <h_gxi@foxmail.com>
2024-06-12 09:34:07 +0800

    生成类型声明文件

--- src/vue-sfc-shim.d.ts
+++ src/vue-sfc-shim.d.ts
@@ -1,6 +1,6 @@
-// declare module '*.vue' {
-//     // eslint-disable-next-line
-//     import { DefineComponent } from 'vue';
-//     const Component: DefineComponent;
-//     export default Component;
-//   }
+declare module '*.vue' {
+    // eslint-disable-next-line
+    import { DefineComponent } from 'vue';
+    const Component: DefineComponent;
+    export default Component;
+  }
